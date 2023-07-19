e619a2b693ab117eca638893ba21c31207e3c9e9
Николай <gulyaev132415@yandex.ru>
2023-07-19 15:53:40 +0500

    1

--- src/main/java/ru/practicum/shareit/exeption/UnsupportedStatusException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exeption;
-
-public class UnsupportedStatusException extends RuntimeException {
-    public UnsupportedStatusException() {
-        super("Unknown state: UNSUPPORTED_STATUS");
-    }
-}
